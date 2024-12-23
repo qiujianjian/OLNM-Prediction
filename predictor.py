effd435ed0ef1f57ec69ed703b4f80b40f24d3c4
zhurunqiu <zhurunqiu1@163.com>
2024-12-23 17:48:52 +0800

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -88,6 +88,8 @@ if st.button("Predict"):
 
 # Calculate SHAP values and display force plot    
     
+    st.subheader("SHAP Force Plot Explanation is {probability:.1f}%.")
+    
     explainer_shap = shap.TreeExplainer(model)    
     
     shap_values = explainer_shap.shap_values(pd.DataFrame([feature_values], columns=feature_names))
@@ -97,6 +99,4 @@ if st.button("Predict"):
     else:        
         shap.force_plot(explainer_shap.expected_value[0], shap_values[:,:,0], pd.DataFrame([feature_values], columns=feature_names), matplotlib=True)    
         
-    plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=1200)    
-       
-    st.image("shap_force_plot.png", caption='SHAP Force Plot Explanation')    
+    plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=1200)        
