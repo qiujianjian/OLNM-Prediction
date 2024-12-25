a4acc357a47d9dc6a9337174741df74aea4551ca
zhurunqiu <zhurunqiu1@163.com>
2024-12-25 14:38:13 +0800

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -65,11 +65,7 @@ if st.button("Predict"):
 
     if predicted_class == 1:
         advice = (
-            f"**According to our model, you have a high risk of OLNM. "
-            f"The model predicts that your probability of having occult lymph node metastasis is {probability:.1f}%. "
-            "While this is just an estimate, it suggests that you may be at significant risk. "
-            "I recommend that you receive neck dissection, this procedure is intended to remove any potentially affected lymph nodes in the neck region, "
-            "which may help prevent the spread of cancer and improve your overall prognosis.**"
+            f"**Based on feature values, predicted probability ofOLNM is {probability:.1f}%. "
         )
     else:
         advice = (
