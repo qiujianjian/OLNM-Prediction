c16c2083fdc4817401ecd95e912ccd9fc2f73037
zhurunqiu <zhurunqiu1@163.com>
2024-12-25 15:29:57 +0800

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -88,9 +88,9 @@ shap_values = explainer_shap.shap_values(pd.DataFrame([feature_values], columns=
 
 def custom_formatter(x):
     return f"{x:.3f}"
-
+    
 if predicted_class == 1:
-    shap.force_plot(
+        shap.force_plot(
         explainer_shap.expected_value[1], 
         shap_values[:,:,1], 
         pd.DataFrame([feature_values], columns=feature_names),
