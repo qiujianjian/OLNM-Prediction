71db377d75f757eb5a1fdc5ba0589cacff558dae
zhurunqiu <zhurunqiu1@163.com>
2024-12-25 15:43:22 +0800

    Update predictor.py

--- predictor.py
+++ predictor.py
@@ -78,19 +78,17 @@ if st.button("Predict"):
 
     # Display advice
     
-    st.markdown(advice, unsafe_allow_html=True)
-
-    # Calculate SHAP values and display force plot 
-
-    explainer_shap = shap.TreeExplainer(model) 
+# Calculate SHAP values and display force plot    
+    
+    explainer_shap = shap.TreeExplainer(model)    
     
     shap_values = explainer_shap.shap_values(pd.DataFrame([feature_values], columns=feature_names))
-    
-        if predicted_class == 1:
-            shap.force_plot(explainer_shap.expected_value[1], shap_values[:,:,1], pd.DataFrame([feature_values], columns=feature_names), matplotlib=True)    
-        else:        
-           shap.force_plot(explainer_shap.expected_value[0], shap_values[:,:,0], pd.DataFrame([feature_values], columns=feature_names), matplotlib=True)    
 
-plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=1200)        
-   
-st.image("shap_force_plot.png", caption='')
+    if predicted_class == 1:
+        shap.force_plot(explainer_shap.expected_value[1], shap_values[:,:,1], pd.DataFrame([feature_values], columns=feature_names), matplotlib=True)    
+    else:        
+        shap.force_plot(explainer_shap.expected_value[0], shap_values[:,:,0], pd.DataFrame([feature_values], columns=feature_names), matplotlib=True)    
+        
+    plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=1200)    
+       
+    st.image("shap_force_plot.png", caption='')   
